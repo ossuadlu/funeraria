4eb58a060333e9d96c38d1f5597530f7d76c1f7c
adny osorio <Huertaslina61@gmail.com>
2022-04-24 16:39:53 -0500

    se crea proyecto funeraria metodologias agiles

---
+++ suscribir.js
@@ -0,0 +1,20 @@
+  function suscribir(){
+    email = document.getElementById("correo electronico").value;
+    val=0;
+
+    if(email==""){
+      val++;
+    }
+
+    if(val== 0){
+      document.getElementById("botonSuscribete").disabled =false;
+    }
+    else {
+      document.getElementById("botonSuscribete").disabled = true;
+    }
+  }
+
+  document.getElementById("email").addEventListener("keyup",suscribir);
+  document.getElementById("botonSuscribete").addEventListener("click",()=>{
+    alert("tu suscripcion se ha realizado")
+  });
